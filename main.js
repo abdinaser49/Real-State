a0f75bf31c7cc24b8d25159516d23518ad9fe52a
Maxamed459 <maxamedmahdi459@gmail.com>
2025-03-04 11:57:59 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let navDiolog = document.getElementById("navDiolog");
+function navHandler() {
+    navDiolog.classList.toggle('hidden');
+}
+// hidding the links when visit via mobile
+function menu_item() {
+    navDiolog.classList.toggle('hidden');
+}
