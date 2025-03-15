d11c67e75844c5281d396e12bff3beff270c5851
Maxamed459 <maxamedmahdi459@gmail.com>
2025-03-15 10:50:36 +0300

    add changes

--- index.js
+++ index.js
@@ -13,18 +13,6 @@ const RentcategoryContainer = document.getElementById("Rent-container");
 
 const buildTheDom = (houses) => {
     houses.forEach((house) => {
-        // let card = `<div class="bg-white shadow-lg rounded-lg overflow-hidden">
-        //         <img src="${house.image}" alt="Property Image" class="w-full h-48 object-cover">
-        //         <div class="p-4">
-        //             <h3 class="text-lg font-semibold text-gray-800">${house.title}</h3>
-        //             <p class="text-gray-600 mt-2">${house.description}</p>
-        //             <div class="mt-4 flex items-center justify-between">
-        //                 <span class="text-xl font-bold text-[#3955E5]">$${house.price}</span>
-        //                 <a href="#" class="text-white bg-[#3955E5] hover:bg-[#2f46bb] px-4 py-2 rounded-lg">View Details</a>
-        //             </div>
-        //         </div>
-        //     </div>`;
-
         let card = `<div class="bg-white shadow-lg rounded-lg overflow-hidden relative">
             <div class="bg-[#222123] rounded absolute right-2 top-2 px-6 py-1"><p class="text-xl text-white">${house.type}</p></div>
             <img src="${house.image}" alt="Property Image" class="w-full h-48 object-cover">
